f535f88b1d915b3f92839df3f351dffc7ba97cbc
Yasuhiro Usutani <y_usutani@me.com>
2011-10-05 18:49:04 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usublog::Application.config.secret_token = '255a151e127c80c596685fc8665676ffea204d50c50c6ea944ec35b45c77f9a008d3d603e6adcdc81339218305d58869fd72391aa83f5af4d9e5afeb4306b38e'
